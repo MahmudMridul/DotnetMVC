7891dab9f5740062d0f6fbbb570e9b5afa568bb9
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-06-14 11:10:55 +0600

    Validation data annotations

--- DotNetMVC/Models/Category.cs
+++ DotNetMVC/Models/Category.cs
@@ -7,8 +7,12 @@ namespace DotNetMVC.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
         [DisplayName("Display Order")]
+        [Range(1,int.MaxValue, ErrorMessage = "Must be greater than 0")]
         public int DisplayOrder { get; set; }
     }
 }
