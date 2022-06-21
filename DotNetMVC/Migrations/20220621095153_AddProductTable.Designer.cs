b6dd765b387ab92db85258d9f9c14eec87661d15
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-06-21 16:18:07 +0600

    New model added to database

--- DotNetMVC/Migrations/20220621095153_AddProductTable.Designer.cs
+++ DotNetMVC/Migrations/20220621095153_AddProductTable.Designer.cs
@@ -3,14 +3,16 @@ using DotNetMVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DotNetMVC.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20220621095153_AddProductTable")]
+    partial class AddProductTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
