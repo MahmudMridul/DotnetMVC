0db22384b89f78364777f6d1e2d4e09c851f4bff
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-06-22 13:08:14 +0600

    ViewModel added

---
+++ DotNetMVC/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+
+namespace DotNetMVC.Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        public Product Product { get; set; }
+        public IEnumerable<SelectListItem> CategorySelectList { get; set; }
+    }
+}
